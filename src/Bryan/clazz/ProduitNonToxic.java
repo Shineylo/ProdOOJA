2902abeadd249899071d68d828ee4f2c5e9045b6
Shineylo <Bryan.artiges@gmail.com>
2022-12-02 13:42:10 +0100

    Adding Products

---
+++ src/Bryan/clazz/ProduitNonToxic.java
@@ -0,0 +1,7 @@
+package Bryan.clazz;
+
+public class ProduitNonToxic extends Produit {
+    public ProduitNonToxic(String nom, int prix, String marque) {
+        super(nom, prix, marque);
+    }
+}
