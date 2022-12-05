350824fce1f132a1aff4a1a0ab8c08c200956a70
Thibault Vroonhove <thibault.vroonhove@edu.technobel.be>
2022-12-05 09:28:55 +0100

    Fix conflicts

--- src/Main.java
+++ src/Main.java
@@ -85,4 +85,26 @@ public class Main {
     private static void afficherStock() {
 
     }
+
+    public static class Outils {
+        public String prixToString(int prix) {
+            return String.format("€ %d.%02d", prix/100, prix%100);
+        }
+
+        // Demande un entier à l'utilisateur et vérifie qu'il est dans l'intervalle [1, nbChoix]
+        public static int getChoixMenu(Scanner sc, int nbChoix) {
+            int choix;
+            do {
+                try {
+                    choix = Integer.parseInt(sc.nextLine());
+                    if(choix > 0 && choix <= nbChoix)
+                        return choix;
+                    else
+                        System.out.println("Erreur: choix inconnu");
+                } catch (NumberFormatException e) {
+                    System.out.println("Erreur: veuillez entrer un nombre entier");
+                }
+            } while(true);
+        }
+    }
 }
